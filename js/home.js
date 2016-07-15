7a79365ea274d8cfb8e676ccc7a76fc7572ce322
Gayan Kalhara <gayan.csnc@gmail.com>
2016-07-15 18:13:14 +0530

    CORS

--- js/home.js
+++ js/home.js
@@ -134,8 +134,7 @@ $('form').submit(function (submitEvt){
       "method": "POST",
       "headers": {
         "content-type": "application/json",
-        "cache-control": "no-cache",
-        "Access-Control-Allow-Origin": "https://www.incypos.com"
+        "cache-control": "no-cache"
       },
       "processData": false,
       "data": JSON.stringify(objJSON),
